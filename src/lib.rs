5b6bb39804b08f0f9ee55b36eb24e6cfc976204b
kpcyrd <git@rxv.cc>
2021-07-12 21:19:37 +0200

    Rename osx to macos

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use anyhow::Result;
 
 mod linux;
 pub use crate::linux::Linux;
-mod osx;
-pub use crate::osx::OSX;
+mod macos;
+pub use crate::macos::MacOS;
 mod windows;
 pub use crate::windows::Windows;
 mod android;
@@ -18,7 +18,7 @@ pub fn detect() -> Result<OsVersion> {
     if cfg!(target_os = "linux") {
         Ok(OsVersion::Linux(Linux::detect()?))
     } else if cfg!(target_os = "macos") {
-        Ok(OsVersion::OSX(OSX::detect()?))
+        Ok(OsVersion::MacOS(MacOS::detect()?))
     } else if cfg!(target_os = "windows") {
         Ok(OsVersion::Windows(Windows::detect()?))
     } else if cfg!(target_os = "android") {
@@ -34,7 +34,7 @@ pub fn detect() -> Result<OsVersion> {
 #[non_exhaustive]
 pub enum OsVersion {
     Linux(Linux),
-    OSX(OSX),
+    MacOS(MacOS),
     Windows(Windows),
     Android(Android),
     OpenBSD(OpenBSD),
@@ -45,7 +45,7 @@ impl ToString for OsVersion {
     fn to_string(&self) -> String {
         match self {
             OsVersion::Linux(v) => v.to_string(),
-            OsVersion::OSX(v) => v.to_string(),
+            OsVersion::MacOS(v) => v.to_string(),
             OsVersion::Windows(v) => v.to_string(),
             OsVersion::Android(v) => v.to_string(),
             OsVersion::OpenBSD(v) => v.to_string(),
